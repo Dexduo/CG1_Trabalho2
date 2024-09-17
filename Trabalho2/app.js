26dda7c24ee2b56ce86bfe7286cdb13772c5a3cf
José Eduardo Nogueira <eduardosoaresn3@gmail.com>
2024-09-17 16:20:17 -0300

    First Commit

---
+++ Trabalho2/app.js
@@ -0,0 +1,16 @@
+var InitDemo = function(){
+    console.log('This is working');
+
+    var canvas = document.getElementById('game-surface');
+    var gl = canvas.getContext('webgl');
+
+    if(!gl){
+        console.log('WebGL not supported, falling back to experimental-webgl');
+        gl = canvas.getContext('experimental-webgl');
+    }
+
+    if(!gl){
+        alert('Your browser does not support WebGL');
+    }
+
+}
