82e1229a3044213281cd775fe90079e3f1e11899
José Eduardo Nogueira <eduardosoaresn3@gmail.com>
2024-09-18 00:57:27 -0300

    Second Commit

---
+++ Trabalho2/README.md
@@ -0,0 +1,6 @@
+Utilizei node e a biblioteca express para criar um servidor e então conseguir "consumir" os arquivos do projeto em WebGL.
+A biblioteca nodemon é apenas para reiniciar o server automaticamente em caso de modificações no código.
+A biblioteca reload para recarregar o browser em caso de modificações.
+
+Para instalar as dependências do projeto utilize "npm install".
+Para executar o projeto utilize "npx nodemon server.js"
--- Trabalho2/app.js
+++ Trabalho2/app.js
@@ -1,5 +1,26 @@
+var vertexShaderText = [
+'precision mediump float;',
+'',
+'attribute vec2 vertPosition;',
+'',
+'void main()',
+'{',
+'   gl_Position = vec4(vertPosition, 0.0, 1.0);',
+'}'
+].join('\n');
+
+var fragmentShaderText = [
+'precision mediump float;',
+'',
+'void main()',
+'{',
+'   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+'}'
+].join('\n')
+
+
 var InitDemo = function(){
-    console.log('This is working');
+    console.log('This is workings');
 
     var canvas = document.getElementById('game-surface');
     var gl = canvas.getContext('webgl');
@@ -12,5 +33,60 @@ var InitDemo = function(){
     if(!gl){
         alert('Your browser does not support WebGL');
     }
+
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderText);
+    gl.shaderSource(fragmentShader, fragmentShaderText);
+
+    gl.compileShader(vertexShader);
+    if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+        console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+        return;
+    }
+
+    gl.compileShader(fragmentShader);
+    if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+        console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+        return;
+    }
+
+    
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if(!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+        return;
+    }
+
+    gl.validateProgram(program);
+    if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS)){
+        console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+        return;
+    }
+
+    // canvas.width = window.innerWidth;
+    // canvas.height = window.innerHeight
+    // gl.viewport(0, 0, window.innerWidth, window.innerHeight)
+
+    // Create Buffer
+
+    var triangleVertices = 
+    [
+        0.0, 0.5,
+        -0.5, -0.5,
+        0.5, -0.5
+    ];
+
+    var triangleVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, triangleVertices, gl.STATIC_DRAW);
 
 }
